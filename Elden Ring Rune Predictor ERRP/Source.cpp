7f127e4cf2a57c84a43e13fff44e1ae2aa39bf52
dgrace6 <dghoochlax57@gmail.com>
2023-03-05 14:12:33 -0500

    App Icon and VS Solution
    Finished the app's icon and also added the vs solution for other people to run it.

--- Elden Ring Rune Predictor ERRP/Source.cpp
+++ Elden Ring Rune Predictor ERRP/Source.cpp
@@ -2,14 +2,14 @@
 #include <cmath>
 #include <iostream>
 #include <wx/spinctrl.h>
-
+#include "resource.h"
 
 
 class App : public wxApp {
 public:
     bool OnInit() {
         wxSizerFlags::DisableConsistencyChecks();
-        wxFrame* window = new wxFrame(NULL, wxID_ANY, "Elden Ring Rune Calculator (ERRP)", wxDefaultPosition, wxSize(600, 500));
+        wxFrame* window = new wxFrame(NULL, wxID_ANY, "Elden Ring Rune Calculator (ERRP)", wxDefaultPosition, wxSize(600, 900));
         wxBoxSizer* sizer = new wxBoxSizer(wxVERTICAL);
 
         wxStaticText* currentLvl = new wxStaticText(window, wxID_ANY, "Your current level:", wxDefaultPosition, wxDefaultSize, wxTE_CENTER);
@@ -24,7 +24,7 @@ public:
         lvlUp->SetFont(wxFont(16, wxFONTFAMILY_DEFAULT, wxFONTSTYLE_NORMAL, wxFONTWEIGHT_BOLD));
         sizer->Add(lvlUp, 0, wxALIGN_LEFT | wxTOP | wxLEFT, 30);
 
-        wxSpinCtrl* inputNumLvl = new wxSpinCtrl(window, wxID_ANY, "1", wxDefaultPosition, wxDefaultSize, wxTE_CENTER);
+        wxSpinCtrl* inputNumLvl = new wxSpinCtrl(window, wxID_ANY, "3", wxDefaultPosition, wxDefaultSize, wxTE_CENTER);
         inputNumLvl->SetFont(wxFont(20, wxFONTFAMILY_DEFAULT, wxFONTSTYLE_NORMAL, wxFONTWEIGHT_NORMAL));
         sizer->Add(inputNumLvl, 0, wxLEFT | wxALIGN_LEFT, 40);
 
@@ -36,21 +36,33 @@ public:
         wxStaticText* resultText = new wxStaticText(window, wxID_ANY, "",
             wxDefaultPosition, wxDefaultSize, wxALIGN_CENTRE_HORIZONTAL);
         resultText->SetFont(wxFont(20, wxFONTFAMILY_DEFAULT, wxFONTSTYLE_NORMAL, wxFONTWEIGHT_NORMAL));
-        sizer->Add(resultText, 1, wxALIGN_CENTER | wxALL | wxEXPAND, 20);
+        sizer->Add(resultText, 0, wxALIGN_LEFT | wxTOP | wxLEFT, 30);
+
+        wxStaticText* runeCostText = new wxStaticText(window, wxID_ANY, "",
+            wxDefaultPosition, wxDefaultSize, wxALIGN_CENTRE_HORIZONTAL);
+        runeCostText->SetFont(wxFont(20, wxFONTFAMILY_DEFAULT, wxFONTSTYLE_NORMAL, wxFONTWEIGHT_NORMAL));
+        sizer->Add(runeCostText, 0, wxALIGN_LEFT | wxTOP | wxLEFT, 30); // Add the rune cost text to the sizer
 
         // Bind button click event to perform calculation
-        calculateButton->Bind(wxEVT_BUTTON, [inputLvl, inputNumLvl, resultText](wxCommandEvent& event) {
+        calculateButton->Bind(wxEVT_BUTTON, [inputLvl, inputNumLvl, window, sizer, runeCostText, resultText](wxCommandEvent& event) {
             float lvl = inputLvl->GetValue(); // Get input value as integer
             float numlvl = inputNumLvl->GetValue(); // Get input value as integer
+            wxString runeCostString;
             float totalRuneCost = 0;
             for (int i = 0; i < numlvl; i++) {
                 float x = ((lvl + 81) - 92) * 0.02;
                 float runeCost = ((x + .1) * ((lvl + 81) * (lvl + 81))) + 1;
                 totalRuneCost = totalRuneCost + runeCost;
+                int roundRuneCost = runeCost;
+                int nextlvl = lvl + 1;
+                runeCostString += wxString::Format("To level %d: %d \n", nextlvl, roundRuneCost);
                 lvl = lvl + 1;
             }
+
+            runeCostText->SetLabelText(wxString::Format("Rune Cost for each level: \n %s", runeCostString));
+
             int result = std::round(totalRuneCost);
-            resultText->SetLabelText(wxString::Format("Result: %d", result)); // Update result text
+            resultText->SetLabelText(wxString::Format("Total Rune Cost: %d", result)); // Update result tex
             });
 
         window->SetSizer(sizer);
